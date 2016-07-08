942ee3c6bdb467b8e16028f204122235ec4e1118
catamo <victor.catamo.rojas@gmail.com>
2016-07-08 11:07:28 -0400

    Maquetacion Mobile de la pagina Index

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,26 +39,19 @@ var imgCount = $("img").length, currentImgCount = 0;
 		}
 	});
 
-	$(".overlay").css("height", $(".overlay").width());
-
-	skel
-		.breakpoints({
-			desktop: '(min-width: 737px)',
-			tablet: '(min-width: 737px) and (max-width: 1200px)',
-			mobile: '(max-width: 736px)'
-		})
-		.viewport({
-			breakpoints: {
-				tablet: {
-					width: 1080
-				}
-			}
-		});
-
 	$(function() {
 
 		var	$window = $(window),
-			$body = $('body');
+			$body = $('body'),
+      $btnMenu = $(".btn-menu"),
+      $navMobile = $("#navMobile");
+
+
+    //Mobile menu toggle
+    $btnMenu.click(function () {
+      $navMobile.toggleClass("active");
+      $body.toggleClass("overflow-block");
+    });
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
@@ -70,55 +63,12 @@ var imgCount = $("img").length, currentImgCount = 0;
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
 
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
-
 		// Dropdowns.
 			$('#nav > ul').dropotron({
 				mode: 'fade',
 				noOpenerFade: true,
 				alignment: 'center'
 			});
-
-		// Off-Canvas Navigation.
-
-			// Title Bar.
-				$(
-					'<div id="titleBar">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
-
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
-
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#titleBar, #navPanel, #page-wrapper')
-						.css('transition', 'none');
 
 	});
 
